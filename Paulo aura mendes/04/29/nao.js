5df27fad35dc35ec7fc5e77836c22833e8d07a2a
joao-victor279 <joaovictor91079177@gmail.com>
2026-04-30 15:33:31 -0300

    atevedade

--- Paulo aura mendes/04/29/nao.js
+++ Paulo aura mendes/04/29/nao.js
@@ -38,4 +38,21 @@ console.log(dobroModerno(4));
 console.log(dobroSimples(2));
 console.log(soma2(2, 3));
 
-//
+//Data
+
+//Atual:
+let data_atual = new Date()
+console.log(data_atual);
+
+//Data especifica:
+let natal = new Date(2025, 11, 25); //0=janeiro \ 11=dezembro
+console.log(natal)
+
+//Componentes:
+console.log(data_atual.getFullYear()); // Ano
+console.log(data_atual.getMonth()); // Mês (0-11)
+console.log(data_atual.getDate()); // Dia do mês
+console.log(data_atual.getDay()); // Dia da semana (0-6, 0 = Domingo)
+console.log(data_atual.getHours()); // Hora
+console.log(data_atual.getMinutes()); // Minutos
+console.log(data_atual.getSeconds()); // Segundos
