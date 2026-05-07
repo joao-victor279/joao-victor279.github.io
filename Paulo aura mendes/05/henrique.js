ea7fe8e71a45febb73373e5df719dcc550b573b3
joao-victor279 <joaovictor91079177@gmail.com>
2026-05-07 08:54:22 -0300

    naoaguentomais

--- Paulo aura mendes/05/henrique.js
+++ Paulo aura mendes/05/henrique.js
@@ -1,6 +1,56 @@
 function sortear(max, min){
-    return Math.floor(Math.random() * (max, min + 1)) + min;
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
-let numeroAleatorio
+let numeroAleatorio = sortear(10, 1)
 let tentativas = 3
+
+function verificar(numeroSelecionado){
+
+    numeroSelecionado = Number(numeroSelecionado)
+
+    if(tentativas <= 0){
+        alert(`Descontrói aí. O número era ${numeroAleatorio}`)
+        return;
+    }
+
+    function desativarBotao(){
+        let botoes = document.querySelectorAll(".botoes")
+        botoes.forEach(botao => botao.disabled = true)
+    }
+    
+    tentativas--;
+
+    if(numeroSelecionado === numeroAleatorio){
+        alert(`Caramba cara. Senti tua aura daqui. Constrói aí man.`)
+        desativarBotao()
+    }
+    else{
+        let dica = "";
+
+        if(numeroSelecionado > numeroAleatorio){
+            dica = `PRA BAIXO!!! O número é menor que ${numeroSelecionado}.`
+        }
+        else{
+            dica = `PRA CIMA!!! O número é maior que ${numeroSelecionado}.`
+        }
+
+        if(tentativas > 0){
+            alert(`${dica} \n Vê se faz direito agora tô te dando ${tentativas} chances`)
+        }
+        else{
+            alert(`sai do curso. O número era ${numeroAleatorio}`)
+            desativarBotao()
+        }
+    }
+}
+
+window.onload = function(){
+    let botoes = document.querySelectorAll(".botoes")
+
+    botoes.forEach(function(button){
+        button.onclick = function(){
+            verificar(this.innerText)
+        }
+    })
+}
