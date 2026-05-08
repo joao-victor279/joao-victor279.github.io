4d4277de7e17cc89a905d26458449f476e7ab3b0
joao-victor279 <joaovictor91079177@gmail.com>
2026-05-08 18:54:11 -0300

    egoson2

--- Egoson/05/atividade_ao.py
+++ Egoson/05/atividade_ao.py
@@ -1,18 +1,18 @@
-# cidadao = {
-#     "nome": input("digite seu nome: "), 
-#     "idade": int(input("Digite sua idade: ")), 
-#     "altura": float(input("Digite sua altura: ")), 
-#     "peso": float(input("Digite seu peso: ")), 
-#     "estado_civil": bool(input("Digite seu estado civil (True para casado e False para solteiro): "))
-# }
-# print(cidadao)
+cidadao = {
+    "nome": input("digite seu nome: "), 
+    "idade": int(input("Digite sua idade: ")), 
+    "altura": float(input("Digite sua altura: ")), 
+    "peso": float(input("Digite seu peso: ")), 
+    "estado_civil": bool(input("Digite seu estado civil (True para casado e False para solteiro): "))
+}
+print(cidadao)
 
-# cidades = ["Fortaleza", "Horizonte", "Aquiraz", "Rio de Janeiro", "Cravinhos"]
-# print(cidades)
-# cidades.append("São Paulo")
-# print(cidades)
-# cidades[1] = "Curitiba"
-# print(cidades)
+cidades = ["Fortaleza", "Horizonte", "Aquiraz", "Rio de Janeiro", "Cravinhos"]
+print(cidades)
+cidades.append("São Paulo")
+print(cidades)
+cidades[1] = "Curitiba"
+print(cidades)
 
 coordenadas = (-16.6711057, -49.270251)
 
