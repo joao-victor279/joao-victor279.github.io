8369e0ae2ba3d792bb9a673fcdb3b199ae04370b
joao-victor279 <joaovictor91079177@gmail.com>
2026-06-18 09:03:06 -0300

    paulix

--- Paulo aura mendes/06/16/script.js
+++ Paulo aura mendes/06/16/script.js
@@ -1,81 +1,85 @@
 let form = document.getElementById("jogoseconsoles");
-let resultado = document.getElementById("id_catalogo")
+let catalogo = document.getElementById("id_catalogo");
 
 let jogos = [];
-
 
 form.addEventListener("submit", (e) =>{
     e.preventDefault();
 
-    let nome = document.getElementById("id_nome").value
+    let nome = document.getElementById("id_jogo").value
     let data = document.getElementById("id_data").value
     let dev = document.getElementById("id_dev").value 
+
 
     let jogo = {
         nome_chave: nome,
         data_chave: data,
-        dev_chave: dev}
+        dev_chave: dev
+    }
+               
+    cadastrarLocalStorage(jogo)
 
-        jogos.push(jogo);
-        console.log(jogos)
+})
 
-        cadastrarLocalStorage("games", jogos)
-
-        listarLocalStorage();
-});
 
 function listarLocalStorage(){
-         
-    function mostrarFila(){
-    id_catalogo.innerHTML = "";
+    catalogo.innerHTML = "";
 
-    let local_storage = localStorage.getItem("games");
+    let local_storage = JSON.parse(localStorage.getItem("games"));
+    console.log(local_storage)
     if(local_storage == null){
-        id_catalogo.innerHTML = `<p>Não há nenhum forte o suficiente.</p>`
+        catalogo.innerHTML += `<p>Não há jogos no catalogo. pobreee</p>`
     }
-    else{
-        jogos = local_storage.split(", ");
+    else {
+        console.log(local_storage)
+
         for(let i = 0; i < jogos.length; i++){
-        id_catalogo.innerHTML +=
-        `<p> ${jogos[i]}  motivadores</p>
+        catalogo.innerHTML += 
+        `<p>N°: ${[i + 1]} | Jogo: ${jogos[i].nome_chave} |Lançamento: ${jogos[i].data_chave} | Dev: ${jogos[i].dev_chave}</p>
 
         <input
         type="button"
-        onclick="editar(${i})"
+        onclick="editar(${[i]})"
         value="Editar">
 
         <input
         type="button"
-        onclick="excluir(${i})"
+        onclick="excluir(${[i]})"
         value="Excluir">
         
-        <br/>`}
+        <br/>`;
+        };
     }
 };
 
-function cadastrarLocalStorage(chave, valor){
-    localStorage.setItem(chave, JSON.stringify(valor))
+function cadastrarLocalStorage(jogo_parametro){
+    jogos.push(jogo_parametro)
+    localStorage.setItem("games", JSON.stringify(jogos))
+    console.log(jogos)
+
+    listarLocalStorage()
 }
 
-for (let i = 0 ; i < jogos.length ; i++ ){
+function editar(indice){
+    
+    jogos[indice].nome_chave = prompt(`Digite o novo jogo: `, jogos[indice].nome_chave);
+    jogos[indice].data_chave = prompt(`Digite a data de lançamento do novo jogo: `, jogos[indice].data_chave);
+    jogos[indice].dev_chave = prompt(`Digite a desenvolvedora do novo jogo: `, jogos[indice].dev_chave);
 
-    function editar(indice){
-    fila_espera[indice] = prompt(`Digite o novo nome: `);
-
-    localStorage.setItem("fila_data", fila_espera.join(", "));
-
-    mostrarFila();
+    localStorage.setItem("games", JSON.stringify(jogos));
+    console.log(indice)
+    listarLocalStorage();
 }
-
+    
 function excluir(indice){
-    fila_espera.splice(indice, 1);
-    if(fila_espera.length == 0){
-        localStorage.removeItem("fila_data")
+    jogos.splice(indice, 1);
+    if(jogos.length == 0){
+        localStorage.removeItem("games")
     }
     else{
-        localStorage.setItem("fila_data", fila_espera.join(", "))
+        localStorage.setItem("games", JSON.stringify(jogos))
     }
 
-    mostrarFila();}
+    listarLocalStorage();
+}
 
-}};
